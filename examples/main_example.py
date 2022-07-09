9dfe79e9e0389f6ed06a4531dda70cef08e8c702
jms1192 <62626527+jms1192@users.noreply.github.com>
2022-07-09 15:20:54 -0400

    Update main_example.py

--- examples/main_example.py
+++ examples/main_example.py
@@ -160,7 +160,7 @@ with st.spinner("Displaying results..."):
     #selected.assign(source='total')
     if not selected_df.empty :
         selected.assign(source='selection')
-        chart_data = pd.concat([df, selected_df])
+        chart_data = pd.concat([selected_df, df])
     else:
         chart_data = df
   
