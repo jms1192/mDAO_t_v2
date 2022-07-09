87715192799c0000b45e76fdc2813d69fd9d9bf6
jms1192 <62626527+jms1192@users.noreply.github.com>
2022-07-09 16:34:18 -0400

    Update main_example.py

--- examples/main_example.py
+++ examples/main_example.py
@@ -148,7 +148,7 @@ grid_response = AgGrid(
 
 df = grid_response['data']
 selected = grid_response['selected_rows']
-selected_df = pd.DataFrame(selected)##.apply(pd.to_numeric, errors='coerce')
+selected_df = pd.DataFrame(selected).apply(pd.to_numeric, errors='coerce')
 
 
 with st.spinner("Displaying results..."):
@@ -161,7 +161,7 @@ with st.spinner("Displaying results..."):
     if not selected_df.empty:
         
         ##chart_data = pd.concat([selected_df, df])
-        chart_data = selected_df.loc['Token', 'sidewinder']
+        chart_data = selected_df.loc['Token', 'USD Amount']
     else:
         chart_data = df
   
