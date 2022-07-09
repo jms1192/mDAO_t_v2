65e466c201fe1c2a84909d5aaed5617831bce6d8
jms1192 <62626527+jms1192@users.noreply.github.com>
2022-07-09 16:28:53 -0400

    Update main_example.py

--- examples/main_example.py
+++ examples/main_example.py
@@ -161,7 +161,7 @@ with st.spinner("Displaying results..."):
     if not selected_df.empty:
         
         ##chart_data = pd.concat([selected_df, df])
-        chart_data = selected_df.loc[['Token', 'sidewinder']]
+        chart_data = selected_df.loc['Token', 'sidewinder']
     else:
         chart_data = df
   
