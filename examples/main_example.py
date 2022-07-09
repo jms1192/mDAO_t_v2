d35974acdd78655b8d9f482514fabafd6bc99766
jms1192 <62626527+jms1192@users.noreply.github.com>
2022-07-09 14:54:33 -0400

    Update main_example.py

--- examples/main_example.py
+++ examples/main_example.py
@@ -148,7 +148,7 @@ grid_response = AgGrid(
 
 df = grid_response['data']
 selected = grid_response['selected_rows']
-selected_df = pd.DataFrame(selected).apply(errors='coerce')
+#selected_df = pd.DataFrame(selected).apply(errors='coerce')
 
 
 with st.spinner("Displaying results..."):
