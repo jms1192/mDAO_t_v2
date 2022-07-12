7c37432c8c482d6ec4049149504e4490a70774ec
jms1192 <62626527+jms1192@users.noreply.github.com>
2022-07-12 18:29:45 -0400

    Update main_example.py

--- examples/main_example.py
+++ examples/main_example.py
@@ -166,7 +166,7 @@ with st.spinner("Displaying results..."):
         chart_data = df
   
     ##chart_data.groupby(['Token']).mean()    
-    st.subheader(chart_data)     
+    st.subheader(chart_data[0])     
     st.dataframe(chart_data)
     chart = alt.Chart(data=chart_data).mark_bar().encode(
         x='Token',
